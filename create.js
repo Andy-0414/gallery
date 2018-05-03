f190c0a7d4181f7daa3fe5ee1cb2580cf5132b26
Andy0414 <pjh8667@naver.com>
2018-05-03 13:50:13 +0900

    ks

---
+++ create.js
@@ -0,0 +1,6 @@
+function createImgBox(){
+    var list = document.getElementById("list");
+    var imgBox = document.createElement("div");
+    imgBox.classList = "imgBox";
+    list.appendChild(imgBox);
+}
